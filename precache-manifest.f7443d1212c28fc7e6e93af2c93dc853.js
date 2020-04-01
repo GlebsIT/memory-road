3681cde7b7533b841f0bc51caaf7c2bb97855586
GlebsIT <belgberez@gmail.com>
2020-04-01 13:12:25 +0400

    Updates

---
+++ precache-manifest.f7443d1212c28fc7e6e93af2c93dc853.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796fb2383b6a92a14f5412490b64d12c",
+    "url": "/memory-road/index.html"
+  },
+  {
+    "revision": "86bc1e108543185296a3",
+    "url": "/memory-road/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "fe98c8c10066dd0f57f0",
+    "url": "/memory-road/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "86bc1e108543185296a3",
+    "url": "/memory-road/static/js/2.fe59cc0e.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/memory-road/static/js/2.fe59cc0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe98c8c10066dd0f57f0",
+    "url": "/memory-road/static/js/main.83235d91.chunk.js"
+  },
+  {
+    "revision": "ebe1d8ebc3059ce52fb0",
+    "url": "/memory-road/static/js/runtime-main.e54b47de.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/memory-road/static/media/persik.4e1ec840.png"
+  }
+]);
